4b4b2b35076c67713a70838372535bbdd10b9c3d
yusyus8016 <124086000+yusyus8016@users.noreply.github.com>
2023-08-10 18:53:54 -0500

    un commit

---
+++ ensayos/clase.js
@@ -0,0 +1,18 @@
+console.log(a)
+
+let a = 10,
+    b = 20,
+    c = "hola"
+    d = "spiderman"
+    x = c + d
+
+    const saludo = c + d
+
+    c = "hola de nuevo"
+
+
+    console.log({a})
+    console.table({a, b, saludo})
+
+    var miVariable = "una variable"
+    console.log(miVariable)
